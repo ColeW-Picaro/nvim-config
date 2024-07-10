01acd2dc2bf76bb40f3ff803dfb7a8becbec9d80
Cole Vohs <colevohs@gmail.com>
2024-07-10 08:43:05 -0400

    made it mine

--- lua/theprimeagen/set.lua
+++ lua/theprimeagen/set.lua
@@ -1,5 +1,3 @@
-vim.opt.guicursor = ""
-
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
@@ -17,10 +15,10 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.hlsearch = false
+vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
-vim.opt.termguicolors = true
+--vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
